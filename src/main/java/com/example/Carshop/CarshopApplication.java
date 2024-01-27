4afa1bc7c81cbf48dc03b0448190987535bea843
ainanyfitiagershom <fitiagershom@yahoo.com>
2024-01-27 21:33:20 +0300

    All back

---
+++ src/main/java/com/example/Carshop/CarshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.Carshop;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+@EntityScan("com.example.Carshop.Model")
+@SpringBootApplication
+public class CarshopApplication {
+
+	public String PORT = System.getenv("PORT");
+	public static void main(String[] args) {
+		SpringApplication.run(CarshopApplication.class, args);
+	}
+}
